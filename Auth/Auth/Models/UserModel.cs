5565eff3ced2672049ecfed8c30beb5c7fc9b6f3
Shalev Tamir <shalevtamir1009@gmail.com>
2024-03-30 12:58:07 +0300

    add validation to user data

--- Auth/Auth/Models/UserModel.cs
+++ Auth/Auth/Models/UserModel.cs
@@ -5,7 +5,9 @@ namespace Auth.Auth.Models
 {
     public class UserModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
